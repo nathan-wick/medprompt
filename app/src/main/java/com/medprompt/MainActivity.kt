4248f1ecc19e8259bbef3bab867ec6597645ef7c
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-06-09 20:43:14 -0400

    Checked if ui is working
    - Added a button to check if my ui is working

--- app/src/main/java/com/medprompt/MainActivity.kt
+++ app/src/main/java/com/medprompt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.medprompt.ui.theme.MedpromptTheme
+import com.medprompt.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +22,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+//                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Staff Engineers")
                 }
             }
         }
@@ -31,13 +33,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Hello $name")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MedpromptTheme {
-        Greeting("Android")
+        Greeting("Staff Engineers")
     }
 }
