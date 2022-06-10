5a962e420ad496dfec9666f9765e4fdad5cae8f9
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-06-09 20:31:15 -0400

    Updated Theme.kt
    - Removed Dark Theme code to simplify code.
    
    - Updated colors that this app will use

--- app/src/main/java/com/medprompt/ui/theme/Theme.kt
+++ app/src/main/java/com/medprompt/ui/theme/Theme.kt
@@ -1,25 +1,17 @@
 package com.medprompt.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue200,
+    primaryVariant = Silver100,
+    secondary = Blue500,
+    background = White,
+    surface = White
 
     /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
@@ -28,12 +20,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MedpromptTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun MedpromptTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
