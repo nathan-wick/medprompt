345ac1f0020e3e7c1d28cfed90611a145e4a55a3
bartrn <89704134+bartrn@users.noreply.github.com>
2022-06-26 20:48:39 -0400

    alignment of var's

--- app/src/main/java/com/medprompt/dto/Medication.kt
+++ app/src/main/java/com/medprompt/dto/Medication.kt
@@ -2,7 +2,9 @@ package com.medprompt.dto
 
 import java.util.*
 //would practice better naming conventions here
-class Medication(doseSize: Int,
+//spaced out doseSize so it aligns 
+class Medication(
+                 doseSize: Int,
                  doseMetricType: Int,
                  stockSize: Int,
                  stockMetricType: Int,
