86f444d16a4139a14647ac7c08d820f864191737
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-06 09:56:22 -0400

    Added navController as param for the cancel button

--- app/src/main/java/com/medprompt/screens/AppointmentScreen.kt
+++ app/src/main/java/com/medprompt/screens/AppointmentScreen.kt
@@ -3,9 +3,10 @@ package com.medprompt.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun AppointmentScreen() {
+fun AppointmentScreen(navController: NavController) {
     Box () {
         Text(text = "Appointment Form")
     }
