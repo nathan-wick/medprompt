d970f8713aba95c7968c4534b5d6337ec4d1ce0d
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-06 20:25:07 -0400

    The date picker is in place..
    took a while for some reason

--- app/src/main/java/com/medprompt/screens/MedicationScreen.kt
+++ app/src/main/java/com/medprompt/screens/MedicationScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -20,10 +19,25 @@ import com.medprompt.ui.theme.MedpromptTheme
 
 @Composable
 fun MedicationScreen(navController: NavController) {
-    Box (
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column {
+        Column (modifier = Modifier.fillMaxSize()) {
+            var day by remember { mutableStateOf("") }
+
+            val monthList = listOf("Jan", "Feb")
+            var monthDropDownOpen by remember { mutableStateOf(false) }
+            var selectedMonthIndex by remember { mutableStateOf(0) }
+
+            val yearList = listOf("2000", "2001", "2002")
+            var yearDropDownOpen by remember { mutableStateOf(false) }
+            var selectedYearIndex by remember { mutableStateOf(0) }
+
+            val timeList = listOf("8:00", "9:00")
+            var timeDropDownOpen by remember { mutableStateOf(false) }
+            var selectedTimeIndex by remember { mutableStateOf(0) }
+
+            val pmAmList = listOf("PM", "AM")
+            var pmAmDropDownOpen by remember { mutableStateOf(false) }
+            var selectedPmAmIndex by remember { mutableStateOf(0) }
+
             Row(modifier = Modifier.padding(5.dp).height(50.dp)) {
                 Button(
                     modifier = Modifier
@@ -45,135 +59,114 @@ fun MedicationScreen(navController: NavController) {
                 TextField(modifier = Modifier.fillMaxWidth(), value = medName, onValueChange = { medName = it })
             }
             Row(modifier = Modifier.padding(5.dp).height(50.dp)) {
-                Column {
-                    var day by remember { mutableStateOf("") }
-
-                    val monthList = listOf("Jan", "Feb")
-                    var monthDropDownOpen by remember { mutableStateOf(false) }
-                    var selectedMonthIndex by remember { mutableStateOf(0) }
-
-                    val yearList = listOf("2000", "2001", "2002")
-                    var yearDropDownOpen by remember { mutableStateOf(false) }
-                    var selectedYearIndex by remember { mutableStateOf(0) }
-                    Row {
-                        Box(modifier = Modifier.weight(3f)) {
-                            TextField(
-                                value = day,
-                                onValueChange = { day = it },
-                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-                            )
-                        }
-                        Box(modifier = Modifier.weight(3f)) {
-                            Text(
-                                monthList[selectedMonthIndex],
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clickable(onClick = { monthDropDownOpen = true })
-                                    .background(Color.Gray))
-                            DropdownMenu(
-                                expanded = monthDropDownOpen,
-                                onDismissRequest = { monthDropDownOpen = false }
-                            ) {
-                                monthList.forEachIndexed { index, month ->
-                                    DropdownMenuItem(onClick = {
-                                        selectedMonthIndex = index
-                                        monthDropDownOpen = false
-                                    }) {
-                                        Text(text = month, textAlign = TextAlign.Center)
-                                    }
-                                }
-                            }
-                        }
-                        Box(modifier = Modifier.weight(3f)) {
-                            Text(
-                                yearList[selectedYearIndex],
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clickable(onClick = { yearDropDownOpen = true })
-                                    .background(Color.Gray))
-                            DropdownMenu(
-                                expanded = yearDropDownOpen,
-                                onDismissRequest = { yearDropDownOpen = false }
-                            ) {
-                                yearList.forEachIndexed { index, year ->
-                                    DropdownMenuItem(onClick = {
-                                        selectedYearIndex = index
-                                        yearDropDownOpen = false
-                                    }) {
-                                        Text(text = year, textAlign = TextAlign.Center)
-                                    }
-                                }
+                Box(modifier = Modifier.weight(3f)) {
+                    TextField(
+                        value = day,
+                        onValueChange = { day = it },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                }
+                Box(modifier = Modifier.weight(3f)) {
+                    Text(
+                        monthList[selectedMonthIndex],
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable(onClick = { monthDropDownOpen = true })
+                            .background(Color.Gray))
+                    DropdownMenu(
+                        expanded = monthDropDownOpen,
+                        onDismissRequest = { monthDropDownOpen = false }
+                    ) {
+                        monthList.forEachIndexed { index, month ->
+                            DropdownMenuItem(onClick = {
+                                selectedMonthIndex = index
+                                monthDropDownOpen = false
+                            }) {
+                                Text(text = month, textAlign = TextAlign.Center)
                             }
                         }
                     }
-
-                    val timeList = listOf("8:00", "9:00")
-                    var timeDropDownOpen by remember { mutableStateOf(false) }
-                    var selectedTimeIndex by remember { mutableStateOf(0) }
-
-                    val pmAmList = listOf("PM", "AM")
-                    var pmAmDropDownOpen by remember { mutableStateOf(false) }
-                    var selectedPmAmIndex by remember { mutableStateOf(0) }
-                    Row {
-                        Box(
-                            modifier = Modifier
-                            .weight(1f)
-                        ) {
-                            Text(
-                                timeList[selectedTimeIndex],
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clickable(onClick = { timeDropDownOpen = true })
-                                    .background(Color.Gray)
-                            )
-                            DropdownMenu(
-                                expanded = timeDropDownOpen,
-                                onDismissRequest = { timeDropDownOpen = false }
-                            ) {
-                                timeList.forEachIndexed { index, time ->
-                                    DropdownMenuItem(onClick = {
-                                        selectedTimeIndex = index
-                                        timeDropDownOpen = false
-                                    }) {
-                                        Text(text = time)
-                                    }
-                                }
-                            }
-                        }
-                        Box(
-                            modifier = Modifier
-                            .weight(1f)
-                        ) {
-                            Text(
-                                pmAmList[selectedPmAmIndex],
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clickable(onClick = { pmAmDropDownOpen = true })
-                                    .background(Color.Gray)
-                            )
-                            DropdownMenu(
-                                expanded = pmAmDropDownOpen,
-                                onDismissRequest = { pmAmDropDownOpen = false }
-                            ) {
-                                pmAmList.forEachIndexed { index, pmAm ->
-                                    DropdownMenuItem(onClick = {
-                                        selectedPmAmIndex = index
-                                        pmAmDropDownOpen = false
-                                    }) {
-                                        Text(text = pmAm)
-                                    }
-                                }
+                }
+                Box(modifier = Modifier.weight(3f)) {
+                    Text(
+                        yearList[selectedYearIndex],
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable(onClick = { yearDropDownOpen = true })
+                            .background(Color.Gray))
+                    DropdownMenu(
+                        expanded = yearDropDownOpen,
+                        onDismissRequest = { yearDropDownOpen = false }
+                    ) {
+                        yearList.forEachIndexed { index, year ->
+                            DropdownMenuItem(onClick = {
+                                selectedYearIndex = index
+                                yearDropDownOpen = false
+                            }) {
+                                Text(text = year, textAlign = TextAlign.Center)
                             }
                         }
                     }
                 }
             }
+            Row(modifier = Modifier.padding(5.dp).height(50.dp)) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text(
+                            timeList[selectedTimeIndex],
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clickable(onClick = { timeDropDownOpen = true })
+                                .background(Color.Gray)
+                        )
+                        DropdownMenu(
+                            expanded = timeDropDownOpen,
+                            onDismissRequest = { timeDropDownOpen = false }
+                        ) {
+                            timeList.forEachIndexed { index, time ->
+                                DropdownMenuItem(onClick = {
+                                    selectedTimeIndex = index
+                                    timeDropDownOpen = false
+                                }) {
+                                    Text(text = time)
+                                }
+                            }
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text(
+                            pmAmList[selectedPmAmIndex],
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clickable(onClick = { pmAmDropDownOpen = true })
+                                .background(Color.Gray)
+                        )
+                        DropdownMenu(
+                            expanded = pmAmDropDownOpen,
+                            onDismissRequest = { pmAmDropDownOpen = false }
+                        ) {
+                            pmAmList.forEachIndexed { index, pmAm ->
+                                DropdownMenuItem(onClick = {
+                                    selectedPmAmIndex = index
+                                    pmAmDropDownOpen = false
+                                }) {
+                                    Text(text = pmAm)
+                                }
+                            }
+                        }
+                    }
+                }
+
         }
-    }
 }
 
 
