f292ba486f03e25a6b0394f516fd7ae8bcb64610
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-08 15:50:06 -0400

    Finished the layout for Form Screen

--- app/src/main/java/com/medprompt/screens/FormScreen.kt
+++ app/src/main/java/com/medprompt/screens/FormScreen.kt
@@ -2,19 +2,18 @@ package com.medprompt.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.medprompt.components.DateTimePicker
-import com.medprompt.components.DropDown
-import com.medprompt.components.HeaderOptions
-import com.medprompt.components.InputField
+import com.medprompt.components.*
 import com.medprompt.ui.theme.MedpromptTheme
 
 @Composable
@@ -46,6 +45,19 @@ fun FormScreen(navController: NavController) {
 
             InputField(weight = 3f, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
             DropDown(weight = 3f, items = freqList)
+        }
+
+        // TODO: Make this a list of dynamic questions
+        Text(text = "Question 1")
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(modifier = Modifier
+                .padding(5.dp)
+                .height(50.dp)) {
+                InputField(weight = 1f, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text))
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Add Question")
+            }
         }
     }
 }
