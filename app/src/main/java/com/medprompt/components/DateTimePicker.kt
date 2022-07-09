60147bd89a567d16d78d8587eb4c741f36c1725d
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-08 22:23:51 -0400

    Deleted some of the unsused packages

--- app/src/main/java/com/medprompt/components/DateTimePicker.kt
+++ app/src/main/java/com/medprompt/components/DateTimePicker.kt
@@ -1,18 +1,11 @@
 package com.medprompt.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.medprompt.ui.theme.MedpromptTheme
@@ -22,6 +15,8 @@ fun DateTimePicker (label: String) {
     Column {
         Text(text = label)
         Row(modifier = Modifier.padding(5.dp).height(60.dp)) {
+
+            // These lists are static data for now... Firebase will come later
             val monthList = listOf("Jan", "Feb")
             val yearList = listOf("2000", "2001", "2002")
 
