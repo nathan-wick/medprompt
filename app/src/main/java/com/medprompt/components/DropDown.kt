747c5681c84ad67af6cc77c522c5399ecdc37953
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-09 16:24:57 -0400

    Minor updates to the DropDown component

--- app/src/main/java/com/medprompt/components/DropDown.kt
+++ app/src/main/java/com/medprompt/components/DropDown.kt
@@ -8,25 +8,29 @@ import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import com.medprompt.ui.theme.MedpromptTheme
 
+/**
+ * Custom Component for our DropDowns, which are used often.
+ */
 @Composable
 fun RowScope.DropDown (weight: Float, items: List<String>) {
     var selectedIndex by remember { mutableStateOf(0) }
     var isOpen by remember { mutableStateOf(false) }
 
-    Box(modifier = Modifier.height(50.dp).weight(weight = weight)) {
+    Box(modifier = Modifier.height(55.dp).weight(weight = weight)) {
         Text(
             items[selectedIndex],
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxSize()
                 .clickable(onClick = { isOpen = true })
-                .background(Color.Gray))
+                .background(color = com.medprompt.ui.theme.Silver100)
+        )
         DropdownMenu(
             expanded = isOpen,
             onDismissRequest = { isOpen = false }
