183e12596ebe5f4f97c72254c0abe829cd3b2f22
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-09 17:20:07 -0400

    Round shape and some color for InputField component

--- app/src/main/java/com/medprompt/components/InputField.kt
+++ app/src/main/java/com/medprompt/components/InputField.kt
@@ -1,8 +1,9 @@
 package com.medprompt.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -12,19 +13,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.medprompt.ui.theme.MedpromptTheme
+import com.medprompt.ui.theme.*
+import com.medprompt.ui.*
 
+/**
+ * A custom component for our text boxes. Here we can customize everything about them.
+ * NOTE: This component has to be inside a Row
+ */
 @Composable
 fun RowScope.InputField (weight: Float, keyboardOptions: KeyboardOptions, placeholder: String = "") {
     var text by remember { mutableStateOf("") }
 
-    Box(modifier = Modifier.weight(weight), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier
+            .height(55.dp)
+            .padding(horizontal = 5.dp)
+            .weight(weight)
+    ) {
         OutlinedTextField(
             value = text,
             onValueChange = { text = it },
             keyboardOptions = keyboardOptions,
             singleLine = true,
-            placeholder = { Text(text = placeholder)}
+            placeholder = { Text(text = placeholder) },
+            shape = CircleShape,
+            modifier = Modifier
+                .fillMaxSize()
+                .border(width = 4.dp, color = Blue200, shape = CircleShape)
         )
     }
 }
