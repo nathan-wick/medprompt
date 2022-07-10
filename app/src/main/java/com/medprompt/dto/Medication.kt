65a75f1b8c7fe1340fd5023706ef74a9bdf03928
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-07-10 10:18:00 -0400

    Updated Medication.kt with default values

--- app/src/main/java/com/medprompt/dto/Medication.kt
+++ app/src/main/java/com/medprompt/dto/Medication.kt
@@ -1,5 +1,7 @@
 package com.medprompt.dto
 
+
+import java.time.LocalDate
 import java.util.*
 /**
  *  A class Medication is noun class Data Transfer Object.
@@ -9,14 +11,14 @@ import java.util.*
  * @property stockSize the stockSize for every Medication : Int
  * @property stockMetricType the stockMetricType for every Medication : Int
  */
-public class Medication(doseSize: Int,
-                 doseMetricType: Int,
-                 stockSize: Int,
-                 stockMetricType: Int,
-                 id: Int,
-                 name: String,
-                 completionDateTime: Date,
-                 frequency: Int
+public class Medication(doseSize: Int = 0,
+                        doseMetricType: Int = 0,
+                        stockSize: Int = 0,
+                        stockMetricType: Int = 0,
+                        id: Int = 0,
+                        name: String = "",
+                        completionDateTime: LocalDate = LocalDate.of(2023, 1, 1),
+                        frequency: Int = 0
 ) : Notification(id, name, completionDateTime, frequency) {
 
 }
