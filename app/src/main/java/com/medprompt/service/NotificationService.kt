5753dea1904903391294eb8ce7743b64ed8b92a6
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-07-10 10:21:15 -0400

    updated NotificationService to inherit from iNotificationService

--- app/src/main/java/com/medprompt/service/NotificationService.kt
+++ app/src/main/java/com/medprompt/service/NotificationService.kt
@@ -2,6 +2,6 @@ package com.medprompt.service
 /**
  * CountryService service package which is business logic and uses coroutines to invoke Retrofit
  */
-class NotificationService {
+class NotificationService : INotificationService {
 
 }
