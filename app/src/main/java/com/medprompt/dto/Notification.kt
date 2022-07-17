9533c8c73b390f5984cd70e92167286068af554e
bartrn <89704134+bartrn@users.noreply.github.com>
2022-07-17 18:21:45 -0400

    added import for date

--- app/src/main/java/com/medprompt/dto/Notification.kt
+++ app/src/main/java/com/medprompt/dto/Notification.kt
@@ -1,7 +1,7 @@
 package com.medprompt.dto
 
 import java.util.*
-
+import java.time.LocalDate
 /**
  * * A class Notification is noun class Data Transfer Object.
  * @property id the id for every Notification : Int
@@ -10,6 +10,7 @@ import java.util.*
  * @property frequency the frequency for every Notification : Int
  *
  */
+
 open class Notification(var id: Int, var name: String, var completionDate: Date, var frequency: Int)
 {
 
