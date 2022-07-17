1ad66dc7ffe4c6696801a2e6c633e6becde3ae9b
bartrn <89704134+bartrn@users.noreply.github.com>
2022-07-17 19:30:16 -0400

    fixed some naming converntions

--- app/src/main/java/com/medprompt/MenuItem.kt
+++ app/src/main/java/com/medprompt/MenuItem.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 data class MenuItem(
     val id: String,
-    val title: String,
-    val contentDescription: String,
-    val icon: ImageVector
+    val Title: String,
+    val ContentDescription: String,
+    val Icon: ImageVector
 )
