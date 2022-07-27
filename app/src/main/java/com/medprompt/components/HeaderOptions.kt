81bf4b4c07bc933c4701ef15811edfb6047eebb4
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-27 18:01:08 -0400

    clean up

--- app/src/main/java/com/medprompt/components/HeaderOptions.kt
+++ app/src/main/java/com/medprompt/components/HeaderOptions.kt
@@ -3,12 +3,9 @@ package com.medprompt.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.medprompt.Screen
