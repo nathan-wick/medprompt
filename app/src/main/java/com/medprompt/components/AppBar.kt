5c0232436d6ecb3a401b280f9d0ac7648b4a325a
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-27 17:50:13 -0400

    clean up and comment

--- app/src/main/java/com/medprompt/components/AppBar.kt
+++ app/src/main/java/com/medprompt/components/AppBar.kt
@@ -4,18 +4,14 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import com.medprompt.R
 
-
+/**
+ * TopBar for our Scaffold on the home screen
+ */
 @Composable
-fun AppBar(
-    onNavigationIconClick: () -> Unit
-) {
+fun AppBar(onNavigationIconClick: () -> Unit) {
     TopAppBar(
-        title = {
-            Text(text = "MedPrompt")
-        },
+        title = { Text(text = "MedPrompt") },
         backgroundColor = MaterialTheme.colors.primary,
         contentColor = MaterialTheme.colors.onPrimary,
         navigationIcon = {
