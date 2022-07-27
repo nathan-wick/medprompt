0190b41ba75dc276d31206048ba5fa4aab3362e2
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-27 14:12:21 -0400

    Renamed and added base temp content

---
+++ app/src/main/java/com/medprompt/screens/edit/EditMedicationScreen.kt
@@ -0,0 +1,26 @@
+package com.medprompt.screens.edit
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.medprompt.AppState
+import com.medprompt.ui.theme.defaultPadding
+
+@Composable
+fun EditMedicationScreen(appState: AppState, documentId: String?) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(defaultPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Edit Apps here")
+        Text(text = "$documentId")
+    }
+}
--- app/src/main/java/com/medprompt/screens/edit/EditMedications.kt
+++
@@ -1,2 +0,0 @@
-package com.medprompt.screens.edit
-
