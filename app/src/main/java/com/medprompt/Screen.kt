c012b26fd49013141537a54f448f2a0e75686efe
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-27 14:08:57 -0400

    Added a edit apps route

--- app/src/main/java/com/medprompt/Screen.kt
+++ app/src/main/java/com/medprompt/Screen.kt
@@ -4,5 +4,6 @@ sealed class Screen(val route: String) {
     object Home: Screen(route = "home_screen")
     object Medication: Screen(route = "medication_screen")
     object Appointment: Screen(route = "appointment_screen")
+    object EditAppointment: Screen(route = "edit_apps_screen")
     object Form: Screen(route = "form_screen")
 }
