e13d2d37a744eb2efc2611eae41cff056cf30348
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-27 09:53:33 -0400

    Updated dto date class

--- app/src/main/java/com/medprompt/dto/Appointment.kt
+++ app/src/main/java/com/medprompt/dto/Appointment.kt
@@ -3,17 +3,10 @@ package com.medprompt.dto
 
 import java.time.LocalDate
 import java.util.*
-/**
- * * A class Appointment is noun class Data Transfer Object. Inherites Notification
- * @property id the id for every Appointment : Int
- * @property name the name for every Appointment : String
- * @property completionDate the completionDate for every Appointment : Date
- * @property frequency the frequency for every Appointment : Int
- */
-class Appointment(id: Int = 0,
-                  name: String = "",
-                  completionDate: LocalDate = LocalDate.of(2023, 1, 1),
-                  frequency: Int = 0
-) : Notification(id, name, completionDate, frequency) {
 
-}
+data class Appointment (
+    val datetime: String,
+    val freqAmount: Number,
+    val freqType: String,
+    val appName: String
+)
